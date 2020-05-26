6dc31a1161ce10486a1e2830d040139eb8ce3714
swaddell7 <stefani.waddell@hotmail.com>
2020-05-26 16:07:13 -0500

    login view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/users', to: 'users#index'
-  post '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show'
-  post '/users/:id', to: 'users#show'
+  # get '/users', to: 'users#index'
+  # post '/users', to: 'users#index'
+  # get '/users/:id', to: 'users#show'
+  # post '/users/:id', to: 'users#show'
+  resources :users
 end
